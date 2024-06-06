64ebc5832c961d84946512c999d1348954161b6c
Bo Cheng <acker0401@outlook.com>
2024-06-06 16:12:41 +0800

    新增port號

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: "PetWorld",
+  port: 3306,
 });
 
 function connect() {

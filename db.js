21f535657d866390725ce8309c6b9803100006cb
Bo Cheng <acker0401@outlook.com>
2024-06-06 16:22:51 +0800

    修正database值

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: "PetWorld",
+  database: "pet-world",
   port: 3306,
 });
 
